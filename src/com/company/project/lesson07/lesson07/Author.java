337d25e2ad3d2517ed9fb888392c63d93e19d1d9
xJ0ySx <joys.devloper@yandex.ru>
2023-10-13 20:08:22 +0300

    refresh git

--- src/com/company/project/lesson07/lesson07/Author.java
+++ src/com/company/project/lesson07/lesson07/Author.java
@@ -2,10 +2,22 @@ package com.company.project.lesson07.lesson07;
 
 public class Author {
 
+    // свойстваю поля, атрибуты
+    //целые числа 0
+    // числа с плавующей точкой 0.0
+    //булевый тип false
+    // ссылочные типы - null
+    public long id;
+    public String name;
+    public boolean test;
+public Author(long authorId){
+    id = authorId;
+}
 
+public Author(long auhotId, String name){
+    id = auhotId;
+    this.name = name;
 
-
-
-
+}
 
 }
--- src/com/company/project/lesson07/lesson07/Lesson07.java
+++ src/com/company/project/lesson07/lesson07/Lesson07.java
@@ -5,6 +5,17 @@ import com.company.project.lesson07.lesson07.Author;
 public class Lesson07 {
 
     public static void main(String[] args){
-        Author author01 = new Author();
+        Author author02 = new Author(2,"Joys");
+        Author author01 = new Author(1);
+
+        System.out.println(author01.id);
+
+        author01.name = "Tom";
+        author01.id = 1;
+        author01.test = true;
+
+        System.out.println(author01.name);
+        System.out.println(author01.test);
+        System.out.println(author02.name);
     }
 }
